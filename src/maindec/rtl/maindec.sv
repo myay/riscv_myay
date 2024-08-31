3bd2e6dd9291b01dc78e434617f7b334e1c85002
Mikail <myayla1245@gmail.com>
2024-09-01 01:21:09 +0200

    Add immediate instructions and documentation

--- src/maindec/rtl/maindec.sv
+++ src/maindec/rtl/maindec.sv
@@ -19,6 +19,7 @@ always_comb
 		7'b1100011: controls = 11'b0_10_0_0_00_1_01_0; // beq
 		7'b0010011: controls = 11'b1_00_1_0_00_0_10_0; // I–type ALU
 		7'b1101111: controls = 11'b1_11_0_0_10_0_00_1; // jal
+		7'b0010011: controls = 11'b1_00_1_0_00_0_10_0; // addi, ori, slti
 	        default: controls = 11'bx_xx_x_x_xx_x_xx_x; // ??? 
 	endcase
 endmodule
